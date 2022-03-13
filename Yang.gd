6687ddf86dc346d335411dab358db97d2602c3c2
Xeadriel <ogulcanasik@web.de>
2022-03-13 19:58:18 +0100

    WIP navigatio2D, changed windows size to a 16:9 ratio

--- Yang.gd
+++ Yang.gd
@@ -4,6 +4,8 @@ var selected = true
 
 var path =  []
 
+const SPEED = 5
+
 onready var nav = get_parent().get_node("Navigation2D")
 
 
@@ -12,7 +14,8 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	moveTowardsPoint()
+	checkIfNextPointReached()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if not selected:
@@ -20,3 +23,17 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_RIGHT and event.pressed:
 			path = nav.get_simple_path(global_position, event.global_position)
+
+func moveTowardsPoint():
+	if not path.empty():
+		return
+	var direction = global_position.direction_to(path[0])
+	global_position += SPEED * direction
+
+func checkIfNextPointReached():
+	pass
+
+
+func compFloats(a, b, epsilon):
+	if a > b:
+		pass
